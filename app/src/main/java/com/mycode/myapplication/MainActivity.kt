37db880a4b4b02870e8d0ba2dc79f2b1e4a92805
Saif Jame <saifjame@Saifs-Air.fios-router.home>
2019-09-11 02:27:23 -0400

    first

--- app/src/main/java/com/mycode/myapplication/MainActivity.kt
+++ app/src/main/java/com/mycode/myapplication/MainActivity.kt
@@ -5,24 +5,30 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
-
-
+import android.util.DisplayMetrics
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        content.layoutParams.width = height+200
+        content.layoutParams.height = height+200
+        blackCircle.layoutParams.height = height/3
+        blackCircle.layoutParams.width = height/3
         val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.shrink)
         val animation2 = AnimationUtils.loadAnimation(applicationContext,R.anim.grow)
         val animation3 = AnimationUtils.loadAnimation(applicationContext,R.anim.growring)
         val animation4 = AnimationUtils.loadAnimation(applicationContext,R.anim.growring2)
         val animation5 = AnimationUtils.loadAnimation(applicationContext,R.anim.growring3)
-        blackCircle.startAnimation(animation2)
-        content.startAnimation(animation)
         blackring.startAnimation(animation3)
         blackring2.startAnimation(animation4)
         blackring3.startAnimation(animation5)
+        blackCircle.startAnimation(animation2)
+        content.startAnimation(animation)
+
     }
 }
